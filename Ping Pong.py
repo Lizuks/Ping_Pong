210c41b103215239fab4184a421d22482fccb788
Lizuks <elizavetaglushko72@gmail.com>
2024-06-20 15:23:03 +0300

    v2
    создан класс для игроков, создано управление, отрисовка игровых персонажей

--- Ping Pong.py
+++ Ping Pong.py
@@ -25,15 +25,20 @@ class Game_sprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(Game_sprite):
-    def update(self):
+    def update_1(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x < 595:
-            self.rect.x += self.speed   
-    def fire(self):
-        bullet = Carrot('carrot.png', self.rect.centerx, self.rect.top, 60, 70, -15)
-        carrots.add(bullet)
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 370:
+            self.rect.y += self.speed  
+    def update_2(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < 370:
+            self.rect.y += self.speed   
+raketka_1 = Player('raketka.png', 0, 200, 70, 130, 5)
+raketka_2 = Player('raketka.png', 630, 0, 70, 130, 5)
 game = True
 finish = False
 while game:
@@ -44,6 +49,9 @@ while game:
         
     if finish != True:
         window.blit(backgroud,(0,0))
-        
+        raketka_1.reset()
+        raketka_1.update_1()
+        raketka_2.reset()
+        raketka_2.update_2()
     display.update()
     clock.tick(FPS)
